80140afc53ed32c31a1fd500657a15926f0f4b80
Mohamadol <mohamadolyaiy@server-large.us-central1-a.c.rock-appliance-396012.internal>
2023-11-28 14:50:30 +0000

    resnet50 data

---
+++ benchmarking/scripts/resnet50.sh
@@ -0,0 +1,23 @@
+
+EXP_NAME="resnet50"
+CORES=32
+
+cd ./../../
+OUT_DIR="./benchmarking/outputs"
+DATA_DIR="./benchmarking/data"
+
+mkdir -p $OUT_DIR
+mkdir -p $DATA_DIR
+
+./benchmarking/scripts/memory_monitor.sh "${DATA_DIR}/memory_usage.csv" &
+pid="$!"
+
+for i in {1..8}
+do
+    echo "running batch id ${i}"
+    ./bin/benchmark $CORES $i > "${OUT_DIR}/${EXP_NAME}.txt"
+done
+
+kill $pid
+
+wait
