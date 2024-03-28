d8cff9f93eabe7af840db8b25c7d72436b3af973
Mohamadol <parinazzhandy@server.us-east1-b.c.decent-courage-412912.internal>
2024-03-28 19:34:59 +0000

    resnet18 _8_8_30_30 swap

--- benchmarking/scripts/resnet18_throughput.sh
+++ benchmarking/scripts/resnet18_throughput.sh
@@ -1,13 +1,15 @@
 
-EXP_NAME="resnet18_throughput_swap"
+SUB_EXP_NAME="/_10_swappiness/_35_dirty_ratio"
+EXP_NAME="resnet18_throughput_swap${SUB_EXP_NAME}"
 CORES=8
 THREADS=8
 MEMORY=30
-TOTAL_BATCHES=3
+TOTAL_BATCHES=4
+
 
 cd ./../../
-OUT_DIR="./benchmarking/outputs/${EXP_NAME}/_${CORES}_${CORES}_${MEMORY}_${MEMORY}/_1__batch_size/_{$TOTAL_BATCHES}__batches"
-DATA_DIR="./benchmarking/data/${EXP_NAME}/_${CORES}_${CORES}_${MEMORY}_${MEMORY}/_1__batch_size/_{$TOTAL_BATCHES}__batches"
+OUT_DIR="./benchmarking/outputs/${EXP_NAME}/_${CORES}_${CORES}_${MEMORY}_${MEMORY}/_1__batch_size/_${TOTAL_BATCHES}__batches"
+DATA_DIR="./benchmarking/data/${EXP_NAME}/_${CORES}_${CORES}_${MEMORY}_${MEMORY}/_1__batch_size/_${TOTAL_BATCHES}__batches"
 
 mkdir -p $OUT_DIR
 mkdir -p $DATA_DIR
@@ -17,7 +19,7 @@ mkdir -p $DATA_DIR
 start=$(date +%s%N)
 
 pids=()
-for ((i=1; i<=BATCH; i++))
+for ((i=1; i<=TOTAL_BATCHES; i++))
 do
     ./bin/benchmark "$THREADS" "$CORES" "$MEMORY" "$EXP_NAME" "$TOTAL_BATCHES" "$i" > "${OUT_DIR}/run_batchid${i}.txt" &
     pids+=($!)
--- server_congif.sh
+++ server_congif.sh
@@ -50,3 +50,15 @@ cd cryptonite_bench
 cmake -S . -B build
 cmake --build build
 sudo cmake --install build
+
+
+sudo mkdir -p /mnt/mohammad/swap
+sudo fallocate -l 64G /mnt/mohammad/swap/swapfile
+sudo chmod 600 /mnt/mohammad/swap/swapfile
+sudo mkswap /mnt/mohammad/swap/swapfile
+sudo swapon /mnt/mohammad/swap/swapfile
+sudo nano /etc/fstab
+/mnt/mohammad/swap/swapfile none swap sw 0 0
+sudo swapon --show
+getconf PAGESIZE
+
